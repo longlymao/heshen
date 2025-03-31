237dcc307ec0f10045d04f14baedd36f9e2bb0e3
longlymao <longly43@163.com>
2025-04-01 00:08:00 +0800

    add rolling-utils

---
+++ rolling-utils/src/filehelper.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 longlymao
+ * All rights reserved.
+ * MIT License
+ */
+
+#include "filehelper.h"
+
+namespace rutils {
+	size_t GetRemainingSize(std::ifstream& stream)
+	{
+		if (!stream) {
+			return 0;
+		}
+		auto pos = stream.tellg();
+		stream.seekg(0, std::ios::end);
+		auto size = stream.tellg();
+		stream.seekg(pos);
+		return size - pos;
+	}
+};
---
+++ rolling-utils/src/filehelper.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 longlymao
+ * All rights reserved.
+ * MIT License
+ */
+
+#pragma	once
+
+#include <fstream>
+
+namespace rutils {
+	size_t GetRemainingSize(std::ifstream& stream);
+};
